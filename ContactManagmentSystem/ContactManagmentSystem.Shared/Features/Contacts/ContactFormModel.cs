3612e6fa9624c56d7d403f6794d32b9611e9f174
Hisham Odeh <hodeh@epitec.com>
2023-06-29 13:59:45 -0400

    Fix edit with validation to preent empty strings and invalid emails and numbers

--- ContactManagmentSystem/ContactManagmentSystem.Shared/Features/Contacts/ContactFormModel.cs
+++ ContactManagmentSystem/ContactManagmentSystem.Shared/Features/Contacts/ContactFormModel.cs
@@ -23,14 +23,30 @@ namespace ContactManagementSystem.Shared.Features.Contacts
         public ContactValidator()
         {
             RuleFor(x => x.FirstName)
-                .MaximumLength(25).WithMessage("First Name must be less that 25 characters")
+                .MaximumLength(25).WithMessage("First Name must be less than 25 characters")
                 .MinimumLength(2).WithMessage("First name must be at least 2 characters")
                 .NotEmpty().WithMessage("First name must not be empty");
+
             RuleFor(x => x.LastName)
-                .MaximumLength(25).WithMessage("Last Name must be less that 25 characters")
+                .MaximumLength(25).WithMessage("Last Name must be less than 25 characters")
                 .MinimumLength(2).WithMessage("Last name must be at least 2 characters")
                 .NotEmpty().WithMessage("Last name must not be empty");
 
+            RuleFor(x => x.PhoneNumber)
+                .MaximumLength(25).WithMessage("Phone Number must be less than 25 characters")
+                .MinimumLength(10).WithMessage("Phone Number must be at least 10 characters")
+                .NotEmpty().WithMessage("Phone Number must not be empty");
+
+            RuleFor(x => x.Email)
+                .MinimumLength(10).WithMessage("Email must be at least 10 characters")
+                .Must(ContainsAtSymbol).WithMessage("Email must contain @ symbol")
+                .NotEmpty().WithMessage("Email must not be empty");
         }
+
+        private bool ContainsAtSymbol(string email)
+        {
+            return email.Contains("@");
+        }
+
     }
 }
